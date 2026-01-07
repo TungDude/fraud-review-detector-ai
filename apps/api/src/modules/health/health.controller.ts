992bdeb199e11efc81dda1f39b849500774ab125
TungDude <krittapop.kms@gmail.com>
2026-01-07 16:12:52 +0700

    feat(health): service layer health check

--- apps/api/src/modules/health/health.controller.ts
+++ apps/api/src/modules/health/health.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "@/helpers/asyncHandler";
+import * as HealthService from "./health.service";
 
 export const healthCheck = asyncHandler(async(_: Request, res: Response) => {
-    res.sendResponse(200, { status: "OK" }, "Service is healthy");
+    const healthStatus = await HealthService.HealthService.check();
+    res.sendResponse(200, healthStatus, "Service is healthy");
 });
