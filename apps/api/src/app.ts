8d3c72148271bf8926deca0ddeb6335078269b0a
TungDude <krittapop.kms@gmail.com>
2026-01-09 23:15:45 +0700

    build(api): add cookie-parser and jsonwebtoken dependencies

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,6 @@
 import express, { type Application } from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { responseFormatterMiddleware } from '@/middlewares/response.middleware';
 import { appRouter } from './routes/appRouter';
 import { config } from '@/config/env';
@@ -10,9 +11,12 @@ export const createApp = (): Application => {
 
     app.use(express.json())
         .use(express.urlencoded({ extended: true }))
+        .use(cookieParser())
         .use(cors(corsOptions))
         .use(responseFormatterMiddleware)
         .use('/api', appRouter);
+    
+    console.log(`CORS:`, corsOptions);
 
     return app;
 }
