a0e7bb40c2bd02f8c2a973823812181628fd0a4c
TungDude <krittapop.kms@gmail.com>
2026-01-09 23:16:17 +0700

    feat(api-middleware): add auth middleware for user verification

---
+++ apps/api/src/middlewares/auth.middleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { ApiError, type IAuth } from "@repo/shared-types";
+import { config } from "@/config/env";
+import jwt from "jsonwebtoken";
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+    const token = req.cookies[config.cookieName];
+
+    if (!token) {
+        throw new ApiError("Authentication token is missing", 401);
+    }
+
+    try {
+        const user = jwt.verify(token, config.jwtSecret) as IAuth;
+
+        if (!user?.userId) {
+            throw new ApiError("Unauthorized", 401);
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(error instanceof Error ? error.message : String(error), 401);
+    }
+}
