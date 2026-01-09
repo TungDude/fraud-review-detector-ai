7a89e1007ce16af3fa2d23d356300a23d0185d0f
TungDude <krittapop.kms@gmail.com>
2026-01-09 23:25:46 +0700

    feat(web-app): implement page routing

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,39 @@
-import { Routes, Route } from 'react-router-dom';
-import HomePage from '@/pages/HomePage';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { PublicRoute, ProtectedRoute } from './app/routes';
+import BaseLayout from './app/layouts/BaseLayout';
+import AppLayout from './app/layouts/AppLayout';
+import AuthenticationPage from './pages/AuthenticationPage';
+import CustomerPage from './pages/customer/CustomerPage';
+import CustomerPostPage from './pages/customer/post/CustomerPostPage';
+import MerchantPage from './pages/merchant/MerchantPage';
+import { PATHS } from './app/routes/paths';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route element={<BaseLayout />}>
+        <Route
+          path={PATHS.auth}
+          element={
+            <PublicRoute>
+              <AuthenticationPage />
+            </PublicRoute>
+          }
+        />
+
+        <Route element={<AppLayout />}>
+          <Route element={<ProtectedRoute roles={["CUSTOMER"]} />}>
+            <Route path={PATHS.customer} element={<CustomerPage />} />
+            <Route path={PATHS.customerPost} element={<CustomerPostPage />} />
+          </Route>
+
+          <Route element={<ProtectedRoute roles={["MERCHANT"]} />}>
+            <Route path={PATHS.merchant} element={<MerchantPage />} />
+          </Route>
+        </Route>
+      </Route>
+
+      <Route path="*" element={<Navigate to={PATHS.auth} replace />} />
     </Routes>
   )
 }
